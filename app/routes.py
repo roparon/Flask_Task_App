e3e49f3c318216b68bc9ff74e2d9fc0acc645123
Aron Rop <aaronrop40@gmail.com>
2025-04-17 14:58:23 +0300

    initial commit

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,4 @@ from app import app
 users = ["rope", "jane", "john", "doe", "alice", "bob", "charlie", "dave", "eve", "frank"]
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', r=users)
