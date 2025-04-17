2f18748e1d89a3a329b75fda6a1bf5df614a8ec9
Aron Rop <aaronrop40@gmail.com>
2025-04-17 14:50:46 +0300

    initial commit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app import app
+
+users = ["rope", "jane", "john", "doe", "alice", "bob", "charlie", "dave", "eve", "frank"]
 @app.route('/')
 def home():
     return render_template('index.html')
