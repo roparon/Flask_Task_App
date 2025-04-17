8822d468c23506465dc17f18b967591015e1ef34
Aron Rop <aaronrop40@gmail.com>
2025-04-17 15:17:35 +0300

    initial commit

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,8 @@
 from flask import render_template
 from app import app
+from app.models import User
 
-users = ["rope", "jane", "john", "doe", "alice", "bob", "charlie", "dave", "eve", "frank"]
-tasks = ["wash the car", "clean the house", "buy groceries", "finish homework", "call mom", "read a book", "exercise", "cook dinner", "write a report", "plan a trip"]
 @app.route('/')
 def home():
-    return render_template('index.html', r=users)
-
-@app.route('/tasks')
-def tasks():
-    return render_template('tasks.html', tasks=tasks)
+    users = User.query.all()
+    return render_template('index.html', users=users)
