b465b297cd0ff3697912e4228f9f3601f72f32fc
Aron Rop <aaronrop40@gmail.com>
2025-04-17 15:42:57 +0300

    initial commit

--- app/models.py
+++ app/models.py
@@ -9,11 +9,8 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
     tasks = db.relationship('Task', backref='user', lazy=True)
 
-    def full_name(self):
-        return f"{self.first_name} {self.last_name}"
-
-    def display_user(self):
-        return f"Full Name: {self.full_name()}"
+def __repr__(self):
+        return f"User(''{self.first_name}', '{self.last_name}')"
 
 
 
