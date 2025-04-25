02840e768698af4fb1da80866e5193b53d5e95ec
Aron Rop <aaronrop40@gmail.com>
2025-04-25 14:18:41 +0300

    innitial commit

--- app/forms.py
+++ app/forms.py
@@ -2,15 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField, SubmitField,PasswordField
 from wtforms.validators import DataRequired
 
-class TaskForm(FlaskForm):
-   title = StringField('Task Title')
-   description = TextAreaField('Task Description')
-   completed = BooleanField('Completed')
-   submit = SubmitField('Submit')
-
 class UserForm(FlaskForm):
    user_name = StringField('User Name', validators=[DataRequired()])
    first_name = StringField('First Name', validators=[DataRequired()])
    last_name = StringField('Last Name', validators=[DataRequired()])
    password = PasswordField('Password', validators=[DataRequired()])
    submit = SubmitField('Register')
+   
+class TaskForm(FlaskForm):
+   title = StringField('Task Title')
+   description = TextAreaField('Task Description')
+   completed = BooleanField('Completed')
+   submit = SubmitField('Submit')
+
+class LoginForm():
+   username = StringField('Username', validators=[DataRequired()])
+   password = PasswordField('Password', validators=[DataRequired()])
+   submit = SubmitField('Login')
