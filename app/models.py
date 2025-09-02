e4fd73ee9957ee447b7247e8133497c1cac78ab7
Aron Rop <aaronrop40@gmail.com>
2025-09-02 19:15:18 +0300

    modified my files

--- app/models.py
+++ app/models.py
@@ -15,8 +15,6 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"{self.first_name}, {self.last_name})"
 
-
-
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
